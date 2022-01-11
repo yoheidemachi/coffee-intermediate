6344159e8e974f592baabf600553840cb2984732
Yohei Demachi <demachiyohei@gmail.com>
2022-01-11 22:12:17 +0900

    add mobile menu

---
+++ js/script.js
@@ -0,0 +1,9 @@
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
